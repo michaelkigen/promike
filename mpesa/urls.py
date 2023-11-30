df1032e7a81e49a129b3a4d78f0f0d128a798c0b
michael <100795405+michaelkigen@users.noreply.github.com>
2023-11-30 07:58:50 +0300

    ne5yf

---
+++ mpesa/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import SubmitView, CheckTransaction, ConfirmView , PaymentTransactionListView,Redeem_points,SearchTransaction
+
+urlpatterns = [
+    path('send/', SubmitView.as_view(), name='submit'),
+    path('confirm/', ConfirmView.as_view(), name='confirm'),
+    path('check-transaction/', CheckTransaction.as_view(), name='check_transaction'),
+    path('transactions/',PaymentTransactionListView.as_view(), name = 'transaction_history' ),
+    path('searchtrans/<int:trans_id>/',SearchTransaction.as_view(), name = 'search_trans' ),
+    path('redeem/',Redeem_points.as_view(),name='redeem_points')
+]
