183d3e5749e80c89c562533ead36325529556866
michael <100795405+michaelkigen@users.noreply.github.com>
2023-12-05 19:09:31 +0300

    allowed header cors headers

--- pro/settings.py
+++ pro/settings.py
@@ -196,7 +196,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
-
+CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
