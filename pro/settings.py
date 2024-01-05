f35522a72d3d598cdb963ee494c50fbea83d5bb1
michael <100795405+michaelkigen@users.noreply.github.com>
2024-01-05 19:43:40 +0300

    adding new functionalities

--- pro/settings.py
+++ pro/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["74.208.98.218"]
 
 
 # Application definition
@@ -152,6 +152,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
