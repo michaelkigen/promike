515dc6c8cc960992bf1a50e65dde99a7f7b678dc
michael <100795405+michaelkigen@users.noreply.github.com>
2024-01-10 17:53:39 +0300

    adding wsgi encoding

--- pro/wsgi.py
+++ pro/wsgi.py
@@ -6,12 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+import sys
+sys.path.append("/home/michael/yahmz/promike")
 import os
 from django.core.wsgi import get_wsgi_application
-import sys
-print("THIS ID THE WSGI FILE THAT GIVE SOME ERROR")
-print("THE PATH",sys.path)
+
+
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro.settings')
 os.environ["DJANGO_SETTINGS_MODULE"] = "pro.settings"
