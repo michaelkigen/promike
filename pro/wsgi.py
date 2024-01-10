23dd0af3b7d5bf3130fb3335fea77d6cdfea7101
michael <100795405+michaelkigen@users.noreply.github.com>
2024-01-10 16:32:37 +0300

    adding wsgi encoding dl

--- pro/wsgi.py
+++ pro/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home/michael/yahmz/promike/pro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro.settings')
 
 application = get_wsgi_application()
   
