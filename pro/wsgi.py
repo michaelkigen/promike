884ea1e76f8b8e59c8afb986eb1c870e3f38e7c5
michael <100795405+michaelkigen@users.noreply.github.com>
2024-01-10 16:57:11 +0300

    adding wsgi encoding dlNFC

--- pro/wsgi.py
+++ pro/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 import sys
+print("THIS ID THE WSGI FILE THAT GIVE SOME ERROR")
 print(sys.path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro.settings')
