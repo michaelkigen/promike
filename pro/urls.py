2dd2f6e6804e759886b3be5c8b10e9e075b9b090
michael <100795405+michaelkigen@users.noreply.github.com>
2024-01-12 14:43:53 +0300

    Update urls.py
    fwesgdvxc

--- pro/urls.py
+++ pro/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 import users
 import Profile
 import menu
-import mpesa
+import payments
 import reciept
 import chatbox
 import records
@@ -13,7 +13,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/',include('users.urls')),
     path('menu/',include('menu.urls')),
-    path('mpesa/',include('mpesa.urls')),
+    path('mpesa/',include('payments.urls')),
     path('reciept/',include('reciept.urls')),
     path('profile/',include('Profile.urls')),
     path('chat/',include('chatbox.urls')),
